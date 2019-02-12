04794559394889c822a18d886aa08752427b5412
Davide <davide.bertolotti@mail.polimi.it>
2019-02-12 14:04:31 +0100

    Added GraphGenerator Class

---
+++ src/main/java/it/polimi/dagsymb/GraphGenerator.java
@@ -0,0 +1,41 @@
+package it.polimi.deepse.dagsymb;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class GraphGenerator {
+public static void createGraph(JavaSparkContext sc, String filename, long size1, long size2, long size3){
+		
+		//Get configuration of Hadoop system
+	    Configuration conf = sc.hadoopConfiguration();
+	    System.out.println("Connecting to -- "+conf.get("fs.defaultFS"));
+	    
+		try {
+			URI uri = URI.create(conf.get("fs.defaultFS") + "/" + filename);
+		    FileSystem fs = FileSystem.get(uri, conf);
+		    OutputStream output = fs.create(new Path(filename));
+		    System.out.println("Output: "+output.toString());
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
+			long bias = 0;
+			if (size1 != size2)
+				bias = size1;
+			for (long i = 1; i <= size1; ++i) {
+		      String line = i + " " + (i + bias);
+	      	  writer.write(line + "\n");
+	        };
+	        writer.flush();
+	        writer.close();
+	        fs.close();
+	    } catch (IOException e) { 
+	    	System.out.println(e.getMessage());
+	    	};
+	}
+}
